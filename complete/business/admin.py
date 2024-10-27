5026a09179c4bb463cf23f15ba712e23b2e1b098
yuwan <jcandaya0616@gmail.com>
2024-10-27 16:59:56 +0800

    102724

--- complete/business/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Business
-from .models import BusinessDetails
-
-@admin.register(Business)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['username', 'password', 'email']
-
-admin.site.register(BusinessDetails)
--- complete/website/templatetags/custom_filters.py
+++
@@ -1,8 +0,0 @@
-# myapp/templatetags/custom_filters.py
-from django import template
-
-register = template.Library()
-
-@register.filter(name='has_group')
-def has_group(user, group_name):
-    return user.groups.filter(name=group_name).exists()
