fa25a9acc310ae4a47d8c0dd47a3746dfd867a43
yuwan <jcandaya0616@gmail.com>
2024-11-11 17:28:29 +0800

    11/11

--- complete/website/urls.py
+++ complete/website/urls.py
@@ -55,7 +55,7 @@ urlpatterns = [
   path('promo/<str:pk>/',views.promo, name="promo"),
   path('success',views.success,name="success"),
   path('resetpass/', auth_views.PasswordResetView.as_view(template_name = "passwordreset.html"), name="reset_password"),
-  path('resetpass_sent/', CustomPasswordResetView.as_view(template_name = "resetemail.html"), name="password_reset_done"),
+  path('resetpass_sent/', auth_views.PasswordResetDoneView.as_view(template_name = "resetemail.html"), name="password_reset_done"),
   path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = "reset.html"), name="password_reset_confirm"),
   path('resetpass_complete/', auth_views.PasswordResetCompleteView.as_view(template_name = "resetdone.html"), name="password_reset_complete"),
 ]
