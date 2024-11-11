bbae1cbe3df8c547a04b4f4c16746393967229ad
yuwan <jcandaya0616@gmail.com>
2024-11-11 14:40:59 +0800

    11/11

--- complete/capstone/settings.py
+++ complete/capstone/settings.py
@@ -128,7 +128,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/static/media'
+MEDIA_URL = '/static/media/'
 MEDIA_ROOT = "/mnt/data/media/media"
 
 AUTHENTICATION_BACKENDS = [
